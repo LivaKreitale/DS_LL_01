83521cb7804608374b1b3125e30e17b8ad9ded16
Līva Kreitāle <liva.kreitale@edu.rtu.lv>
2025-02-23 16:35:19 +0200

    Update LL_1.py

--- LL_1.py
+++ LL_1.py
@@ -22,20 +22,37 @@ class LinkedList:
         return True
         
 
-    # WRITE FIND_MIDDLE_NODE METHOD HERE #
-    #                                    #
-    #                                    #
-    #                                    #
-    #                                    #
-    ######################################
+    def find_middle_node(self):
+        slow = self.head
+        fast = self.head
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        return slow
+    
+def valid_numbers(prompt):
+    num_str = input(prompt).strip()
+    num_list = num_str.split()
+    while not all(num.isdigit() for num in num_list):
+        print("invalid input")
+        num_str = input(prompt).strip()
+        num_list = num_str.split()
+    return [int(num) for num in num_list]
+
+num_nodes = valid_numbers("Enter the numbers ")
+if len(num_nodes) > 0:
+    my_linked_list = LinkedList(num_nodes[0])
+    
+    for value in num_nodes[1:]:
+        my_linked_list.append(value)
+    
+    print("Middle Node :", my_linked_list.find_middle_node().value)
+else:
+    print("enter atleast one number.")
 
 
 
-my_linked_list = LinkedList(1)
-my_linked_list.append(2)
-my_linked_list.append(3)
-my_linked_list.append(4)
-my_linked_list.append(5)
 
-print( my_linked_list.find_middle_node().value )
 
